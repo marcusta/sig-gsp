8e204fe9d0adf063e03ca6f6bf863ee8744f98b6
Marcus Andersson <marcust@axis.com>
2024-11-19 13:39:39 +0100

    Add logging around migration

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,7 +3,16 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
-const sqlite = new Database("./data/gspro.db");
-const db = drizzle(sqlite);
+export async function migrateDatabase() {
+  console.log("Migrating database");
+  const sqlite = new Database("./data/gspro.db");
+  const db = drizzle(sqlite);
+  try {
+    await migrate(db, { migrationsFolder: "./src/db/migrations" });
+    console.log("Database migrated");
+  } catch (error) {
+    console.error("Error migrating database", error);
+  }
+}
 
-await migrate(db, { migrationsFolder: "./src/db/migrations" });
+migrateDatabase();
